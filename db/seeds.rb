452219cfeb15668f0ffadf8244af307bd6fe04e0
Lucian Tarna <skillvendor193@gmail.com>
2017-02-05 05:56:24 +0200

    create master role on heroku

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+  @admin = Admin.new(email: 'diana.daia@yahoo.com', role: 'Master')
+  @admin.password = '1234567'
+  @admin.save
